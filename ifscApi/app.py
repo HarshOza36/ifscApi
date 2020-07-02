75c72d5ec19a239ce2954fa7f8fb1c563bae2d4b
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2020-07-02 14:10:32 +0530

    Update app.py

--- ifscApi/app.py
+++ ifscApi/app.py
@@ -20,7 +20,7 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
-    except Error as e:
+    except Exception as e:
         print(e)
 
     return conn
