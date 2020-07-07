c1050c49edd3f8e21fbe43d5705300ccf93fe1b6
HarshOza36 <harshoza3636@gmail.com>
2020-07-07 11:16:39 +0530

    Testing

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup,find_namespace_packages,find_packages
+from setuptools import setup, find_namespace_packages, find_packages
 setup(name='ifscApi',
-      version='0.0.1',
+      version='0.1',
       description='Api for Ifsc',
-      author='harsh',
+      author='Harsh Oza',
       author_email='harshoza3636@gmail.com',
       license='MIT',
       install_requires=[
@@ -10,14 +10,20 @@ setup(name='ifscApi',
           'flask-restful'
       ],
       url="https://github.com/HarshOza36/ifsc_api",
-    packages=find_packages(),
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
-  )
-#       packages=find_namespace_packages(include=['ifscApi.*']),
-#       zip_safe=False)
-# ['ifscApi']
+      download_url='https://github.com/HarshOza36/ifscApi/archive/v_01.tar.gz',
+      keywords=['IFSC Code', 'IFSC', 'Bank IFSC Codes'],
+      packages=find_packages(),
+      include_package_data=True,
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Topic :: Software Development :: Build Tools",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.7",
+          "Operating System :: OS Independent",
+      ],
+      )
