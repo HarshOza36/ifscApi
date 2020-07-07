11c13454048b933f1a5fbae6dcb0ba212ff2abfe
HarshOza36 <harshoza3636@gmail.com>
2020-07-07 11:35:46 +0530

    Publish 1

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_namespace_packages, find_packages
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(name='ifscApi',
       version='0.1',
       description='Api for Ifsc',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Harsh Oza',
       author_email='harshoza3636@gmail.com',
       license='MIT',
